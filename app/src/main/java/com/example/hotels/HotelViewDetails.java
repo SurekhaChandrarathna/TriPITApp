dc6ac8a53274ecd4bf30ad802d9d578dcba9d45b
SurekhaChandrarathna <IT19132938>
2020-09-28 02:58:34 +0530

    Hotels update

---
+++ app/src/main/java/com/example/hotels/HotelViewDetails.java
@@ -0,0 +1,69 @@
+package com.example.hotels;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.example.hotels.Model.Hotels;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.squareup.picasso.Picasso;
+
+public class HotelViewDetails extends AppCompatActivity
+{
+    private ImageView hotelDetailImage;
+    private TextView hotelDetailName , hotelDetailAddress ,hotelDetailAbout;
+    private String HotelName = "";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hotel_view_details);
+
+        hotelDetailImage = (ImageView)findViewById(R.id.hotel_detail_image);
+        hotelDetailName= (TextView)findViewById(R.id.hotel_detail_name);
+        hotelDetailAddress= (TextView)findViewById(R.id.hotel_detail_address);
+        hotelDetailAbout= (TextView)findViewById(R.id.hotel_detail_description);
+
+        ShowHotelDetails();
+
+    }
+
+    private void ShowHotelDetails()
+    {
+        DatabaseReference hotelReference = FirebaseDatabase.getInstance().getReference().child("Hotels");
+
+        hotelReference.child(HotelName).addValueEventListener(new ValueEventListener()
+        {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot)
+            {
+                if(snapshot.exists())
+                {
+                    Hotels hotels = snapshot.getValue(Hotels.class);
+
+                    hotelDetailName.setText(hotels.getHotelName());
+                    hotelDetailAddress.setText(hotels.getHotelAddress());
+                    hotelDetailAbout.setText(hotels.getAboutHotel());
+                    Picasso.get().load(hotels.getHotelimage()).into(hotelDetailImage);
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error)
+            {
+
+            }
+        });
+
+
+    }
+
+}
--- app/src/main/java/com/example/hotels/Model/Hotels.java
+++ app/src/main/java/com/example/hotels/Model/Hotels.java
@@ -2,19 +2,20 @@ package com.example.hotels.Model;
 
 public class Hotels
 {
-    private String HotelName, AboutHotel, HotelAddress, HotelContacts;
+    private String HotelName, AboutHotel, HotelAddress, HotelContacts, Hotelimage;
 
     public Hotels()
     {
 
     }
 
-    public Hotels(String HotelName, String AboutHotel, String HotelAddress, String HotelContacts)
+    public Hotels(String HotelName, String AboutHotel, String HotelAddress, String HotelContacts ,String Hotelimage)
     {
         this.HotelName = HotelName;
         this.AboutHotel = AboutHotel;
         this.HotelAddress = HotelAddress;
         this.HotelContacts = HotelContacts;
+        this.Hotelimage= Hotelimage;
     }
 
     public String getHotelName() {
@@ -47,5 +48,13 @@ public class Hotels
 
     public void setHotelContacts(String hotelContacts) {
         HotelContacts = hotelContacts;
+    }
+
+    public String getHotelimage() {
+        return Hotelimage;
+    }
+
+    public void setHotelimage(String hotelimage) {
+        Hotelimage = hotelimage;
     }
 }
