0e964f7bdb994c349f210427a8bb63b729feb4a4
SurekhaChandrarathna <IT19132938>
2020-10-01 16:09:03 +0530

    Places update new

---
+++ app/src/main/java/com/example/tripit/PlaceListAdmin.java
@@ -0,0 +1,81 @@
+package com.example.tripit;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.tripit.Model.Places;
+import com.example.tripit.ViewHolder.PlaceViewHolder;
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.firebase.ui.database.FirebaseRecyclerOptions;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.squareup.picasso.Picasso;
+
+public class PlaceListAdmin extends AppCompatActivity
+{
+    private DatabaseReference PlacesRef;
+    private RecyclerView recyclerView;
+    RecyclerView.LayoutManager layoutManager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_place_list_admin);
+
+        PlacesRef= FirebaseDatabase.getInstance().getReference().child("Places");
+
+        recyclerView = findViewById(R.id.recycler_menu);
+        recyclerView.setHasFixedSize(true);
+        layoutManager= new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(layoutManager);
+
+    }
+
+    @Override
+    protected void onStart()
+    {
+        super.onStart();
+
+        FirebaseRecyclerOptions<Places> options = new FirebaseRecyclerOptions.Builder<Places>()
+                .setQuery(PlacesRef,Places.class).build();
+
+
+        FirebaseRecyclerAdapter<Places , PlaceViewHolder> adapter = new FirebaseRecyclerAdapter<Places, PlaceViewHolder>(options)
+        {
+            @Override
+            protected void onBindViewHolder(@NonNull PlaceViewHolder holder, int position, @NonNull final Places model)
+            {
+                holder.txtPlaceName.setText(model.getName());
+                holder.txtPlaceAddress.setText(model.getAddress());
+                Picasso.get().load(model.getImage()).into(holder.imageView);
+
+
+
+            }
+
+            @NonNull
+            @Override
+            public PlaceViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType)
+            {
+                View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.place_layout, parent,false);
+                PlaceViewHolder holder= new PlaceViewHolder(view);
+
+                return holder;
+            }
+        };
+
+        recyclerView.setAdapter(adapter);
+        adapter.startListening();
+    }
+
+
+}
--- app/src/main/java/com/example/tripit/PlacesHome.java
+++ app/src/main/java/com/example/tripit/PlacesHome.java
@@ -6,10 +6,12 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 
 public class PlacesHome extends AppCompatActivity {
 
-    Button AddPlacesButton ;
+    Button AddPlacesButton, ViewPlaceButton ;
+    ImageButton BackToHomeButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +19,8 @@ public class PlacesHome extends AppCompatActivity {
         setContentView(R.layout.activity_places_home);
 
         AddPlacesButton= (Button)findViewById(R.id.btn_admin_add_places);
+        ViewPlaceButton= (Button) findViewById(R.id.btn_admin_view_places);
+        BackToHomeButton= (ImageButton)findViewById(R.id.admin_backto_home);
 
         AddPlacesButton.setOnClickListener(new View.OnClickListener()
         {
@@ -24,6 +28,25 @@ public class PlacesHome extends AppCompatActivity {
             public void onClick(View view)
             {
                 Intent intent= new Intent(PlacesHome.this,AddPlacesActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        ViewPlaceButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v)
+            {
+                Intent intent= new Intent(PlacesHome.this,PlaceListAdmin.class);
+                startActivity(intent);
+            }
+        });
+
+
+        BackToHomeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v)
+            {
+                Intent intent= new Intent(PlacesHome.this,AdminHome.class);
                 startActivity(intent);
             }
         });
